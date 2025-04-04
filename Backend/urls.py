e1e309d12f9f9643f7b294c9de5ea9a17176c764
saroj <saroj1229@xavier.edu.np>
2025-04-04 16:44:30 +0545

    More to add

--- Backend/urls.py
+++ Backend/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from Backend import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("about-us/", views.aboutUs),
 ]
