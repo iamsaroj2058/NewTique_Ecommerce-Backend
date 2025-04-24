f00feb2d5a1b64975d854e0b3ee4fd56fd37ca2b
saroj <saroj1229@xavier.edu.np>
2025-04-24 17:30:34 +0545

    Django restart

--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from store.serializers import ProductSerializer
-# Create your views here.
 from rest_framework import viewsets
 from .models import Product
-
+from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__name=category)
+        return queryset
